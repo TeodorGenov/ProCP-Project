2d6e8b2b165c877c783bc9fd8cd4bdaeb189f174
Yoanna <yoanna.i.borisova@gmail.com>
2018-03-28 00:29:51 +0200

    Added Airspace, WeatherConditions and RainType sln, waiting for other classes/details to finish it

---
+++ ProCp Airspace, WeatherConditions, RainType/ProCp/ProCp/Airspace.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProCp
+{
+    class Airspace
+    {
+        public Airport Airport
+        { get; set}
+
+        public Checkpoint[] AvailableCheckpoints
+        { get; set; }
+
+        public Airplane[] AirplanesInTheAir
+        { get; set; }
+
+        public WeatherConditions WeatherConditions
+        { get; set; }
+
+        public Airspace(Airport airport, Checkpoint[] availableCheckpoints, Airplane[] airplanesInTheAir, WeatherConditions weatherConditions)
+        {
+            Airport = airport;
+            AvailableCheckpoints = availableCheckpoints;
+            AirplanesInTheAir = airplanesInTheAir;
+            WeatherConditions = weatherConditions;
+        }
+
+        public void AddAirplane()
+        {
+
+        }
+
+        public void AddCheckpoint(double x, double y)
+        {
+
+        }
+
+        public void RemoveCheckpoint(double x, double y)
+        {
+
+        }
+
+        public void ChangeWeatherConditions(WeatherConditions newWeatherConditions)
+        {
+            WeatherConditions = newWeatherConditions;
+        }
+
+
+
+
+    }
+}
---
+++ ProCp Airspace, WeatherConditions, RainType/ProCp/ProCp/WeatherConditions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProCp
+{
+    public class WeatherConditions
+    {
+
+
+        public double Humidity
+        { get; set; }
+
+        public double WindSpeed
+        { get; set; }
+
+        public double WindDIrection
+        { get; set; }
+
+        public double Visibility
+        { get; set; }
+
+        public double TemperatureC
+        { get; set; }
+
+        public RainType RainType
+        { get; set; }
+
+        public double RainIntensity
+        { get; set; }
+
+
+        public WeatherConditions(double humidity, double windSpeed, double windDirection, double visibility, double temperatureC, RainType rainType, double rainIntensity)
+        {
+            Humidity = humidity;
+            WindSpeed = windSpeed;
+            WindDIrection = windDirection;
+            Visibility = visibility;
+            TemperatureC = temperatureC;
+            RainType = rainType;
+            RainIntensity = rainIntensity;
+        }
+
+    }
+} 
