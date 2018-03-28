4fe14b7275b07ad4a0c7488106e37247598de1bd
Rostislav Tinchev <rostislav.tinchev@gmail.com>
2018-03-28 08:32:55 +0200

    added some classes

---
+++ Air Traffic Simulation/Air Traffic Simulation/Checkpoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Air_Traffic_Simulation
+{
+    class Checkpoint : ICheckpoint
+    {
+        private string name;
+        private double coordinateX;
+        private double coordinateY;
+
+        public double CoordinateX()
+        {
+            
+        }
+
+        public double CoordinateY()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Name()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Air Traffic Simulation/Air Traffic Simulation/ICheckpoint.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Air_Traffic_Simulation
+{
+    interface ICheckpoint
+    {
+        string Name();
+        double CoordinateX();
+        double CoordinateY();
+    }
+}
