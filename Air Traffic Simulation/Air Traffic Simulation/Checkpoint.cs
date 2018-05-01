3c15f05cf041ceb0b06d59a744f0ebaaa75061ca
Rostislav Tinchev <rostislav.tinchev@gmail.com>
2018-05-01 16:29:07 +0200

    Application updated with all of the other branches, missing documentation added, Design Document updated with Sequence Diagram

--- Air Traffic Simulation/Air Traffic Simulation/Checkpoint.cs
+++ Air Traffic Simulation/Air Traffic Simulation/Checkpoint.cs
@@ -6,17 +6,25 @@ using System.Threading.Tasks;
 
 namespace Air_Traffic_Simulation
 {
+    [Serializable]
     class Checkpoint : AbstractCheckpoint
     {
         public override string Name { get; }
         public override double CoordinateX { get; }
         public override double CoordinateY { get; }
+        public override LinkedList<AbstractCheckpoint> ShortestPath { get; set; }
+        public override double DistanceFromSource { get; set; }
+        public override Dictionary<AbstractCheckpoint, double> ReachableNodes { get; set; }
 
         public Checkpoint(string name, double coordinateX, double coordinateY)
         {
             this.Name = name;
             this.CoordinateX = coordinateX;
-            this.CoordinateY = CoordinateY;
+            this.CoordinateY = coordinateY;
+
+            ShortestPath = new LinkedList<AbstractCheckpoint>();
+            DistanceFromSource = Int32.MaxValue;
+            ReachableNodes = new Dictionary<AbstractCheckpoint, double>();
         }
 
     }
