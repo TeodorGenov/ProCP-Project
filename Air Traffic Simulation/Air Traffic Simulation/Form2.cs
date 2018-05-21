b004d9b61ace92b7a341da8a59e31ddf708daf4d
Yoanna <yoanna.i.borisova@gmail.com>
2018-05-22 00:01:32 +0200

    Added log in

--- Air Traffic Simulation/Air Traffic Simulation/Form2.cs
+++ Air Traffic Simulation/Air Traffic Simulation/Form2.cs
@@ -12,11 +12,16 @@ namespace Air_Traffic_Simulation
 {
     public partial class Form2 : Form
     {
+        Database dbConnection;
+        bool loggedIn;
+
         public Form2()
         {
             InitializeComponent();
-            bunifuMaterialTextbox1.Text = "";
-            bunifuMaterialTextbox2.Text = "";
+            bunifuMaterialTextbox1.Text = "Name";
+            bunifuMaterialTextbox2.Text = "Password";
+            loggedIn = false;
+            dbConnection = new Database();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -33,6 +38,17 @@ namespace Air_Traffic_Simulation
             var newForm = new Form1();
             newForm.Show();
             this.Hide();
+
+            if (dbConnection.GetLogin(bunifuMaterialTextbox1.Text, bunifuMaterialTextbox1.Text) == true)
+            {
+                MessageBox.Show("Login successfull.");
+                loggedIn = true;
+
+            }
+            else
+            {
+                MessageBox.Show("Login denied.");
+            }
         }
 
         private void bunifuGauge1_Load(object sender, EventArgs e)
